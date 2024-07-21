34026e253ab19b59a2b25075391f1591b6271927
Erryo <159823096+Erryo@users.noreply.github.com>
2024-07-21 15:31:22 +0200

    Smth to smth

--- api/html.go
+++ api/html.go
@@ -3,6 +3,7 @@ package api
 import (
 	"time"
 
+	"github.com/Erryo/Infys-Universe/types"
 	"github.com/Erryo/Infys-Universe/ui"
 	"github.com/Erryo/Infys-Universe/ui/about"
 	"github.com/Erryo/Infys-Universe/ui/user"
@@ -32,11 +33,12 @@ func ShowSignIn(c echo.Context) error {
 func ShowHome(c echo.Context) error {
 	username := GetClaim(c)
 	if isTimeOk(10, c) {
-		getWeather("", "Goettingen")
+		_, weather := getWeather("", "Goettingen")
+		weather.Name = "Goettingen"
 		c.SetCookie(createTimeCookie(time.Now().Format(time.RFC3339)))
+		return Render(c, user.Home(username, weather))
 	}
-
-	return Render(c, user.Home(username))
+	return Render(c, user.Home(username, types.CityData{}))
 }
 
 func ShowTTT(c echo.Context) error {
